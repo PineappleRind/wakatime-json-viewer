379c575ee396fda74d45bbf9fed367f925cebab9
pr <pineapplerind.info@gmail.com>
2023-03-22 11:13:56 -0400

    feat: base infrastructure
    Revert "feat: base infrastructure"
    
    This reverts commit b97078cda4f15085878e5912bf4b2da113354d86.
    
    Revert "Revert "feat: base infrastructure""
    
    This reverts commit c4e3173e964fd92c05dae2b508a7076b8dc873c9.
    
    æ

---
+++ src/panels/index.ts
@@ -0,0 +1,6 @@
+import AllTime from "./AllTime.astro";
+import Machines from "./Machines.astro";
+import Languages from "./Languages.astro";
+
+
+export default { alltime: AllTime, machines: Machines, languages: Languages };
